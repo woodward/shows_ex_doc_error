a2953a30ae8b09d589bf6a668400f0ca79daa0ed
Greg Woodward <woodward@users.noreply.github.com>
2023-04-07 15:10:58 -0700

    Add some KaTeX math equations to the main module docs

--- lib/shows_ex_doc_error.ex
+++ lib/shows_ex_doc_error.ex
@@ -1,6 +1,18 @@
 defmodule ShowsExDocError do
   @moduledoc """
-  Documentation for `ShowsExDocError`.
+  This should show up as math equations:
+
+  $$  F = M A $$
+
+  or an inline equation via a single dollar sign, such as $ F = M A $
+
+  Some more equations:
+
+  $$ f(x) =  \\int_{-\\infty}^\\infty
+    f\\hat(\\xi)\\,e^{2 \\pi i \\xi x}
+    \\,d\\xi $$
+
+  If you're seeing dollar signs and gibberish, KaTeX is not set up properly!
   """
 
   @doc """
